02636ee1cc8cc2f6a0bd14b9d488f281a91dcf7f
Jared Bustamante <jaredbusta@gmail.com>
2022-09-25 18:47:19 -0700

    botton navigator bar

---
+++ lector_qr_jb/lib/nfc/screens/nfc_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class NfcScreen extends StatelessWidget {
+  const NfcScreen({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("aqui irá lo del nfc"),
+    );
+  }
+}
---
+++ lector_qr_jb/lib/qr/screens/qr_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class QrScreen extends StatelessWidget {
+  const QrScreen({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("Aqui va ir toodo lo del Qr"),
+    );
+  }
+}
